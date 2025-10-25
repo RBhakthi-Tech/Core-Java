8d0848e2cf70961774cf1d84736fe8c9020e1373
Veerendra Babu <Veerendra Babu@DESKTOP-7B6125K>
2025-10-25 23:37:09 +0530

    fourth

---
+++ Corejavaprogram/src/com/tnsif/superkeyworld/Superdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.superkeyworld;
+
+public class Superdemo {
+int a=60;// variable
+	
+	void drinking() {//method
+		System.out.println("tea");
+	}
+	 Superdemo(){ // constructor 
+		 System.out.println("welcome to java");
+	 }
+
+}
