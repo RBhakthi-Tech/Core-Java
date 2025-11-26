cf85492ae19ccd3432bd7009902c5d0733725fd5
Veerendra Babu <Veerendra Babu@DESKTOP-7B6125K>
2025-11-26 23:35:09 +0530

    java

---
+++ Corejavaprogram/src/com/tnsif/stringsprogram/Equalsdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.stringsprogram;
+
+public class Equalsdemo {
+	public static void main(String[] args) {
+		String s1="sachin";
+		String s2="sachin";
+		String s3=new String("sachin");
+		String s4="saurav";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s4));
+		System.out.println(s1.equals(s3));
+		
+	}
+
+}
